5a99b300d9f3ebe98a3c92903b44fb5f4ddff35d
thisclinton <thisclinton@gmail.com>
2024-01-20 14:42:53 +0100

    contact component created

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,11 @@
+import "./contact.css";
+
+function Contact() {
+  return (
+    <section className="client">
+      <h2 className="client__title">Some of our notable Clients</h2>
+    </section>
+  );
+}
+
+export default Contact;
