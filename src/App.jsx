f2b9cb120760e80aee86994dfa2eba81603e5ebe
thisclinton <thisclinton@gmail.com>
2024-01-20 10:38:14 +0100

    added the gallery component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
 import Footer from "./components/Footer/Footer";
 import Preloader from "./components/PreLoader/Preloader";
+import Gallery from "./components/Gallery/Gallery";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -16,6 +17,7 @@ function App() {
         <Nav />
         <Hero />
         <About />
+        <Gallery />
         <Footer />
       </div>
     </>
