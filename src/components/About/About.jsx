dc9cffce477a07273ed883593fa20ea932ba1840
thisclinton <thisclinton@gmail.com>
2024-01-29 02:41:58 +0100

    worked on the suppliers section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,20 +4,76 @@ import { useEffect, useRef } from "react";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 import SectionHeader from "../SectionHeader";
 
+const suppliersData = [
+  {
+    title: "Fenner Dunlop",
+    description:
+      "Fenner Dunlop is a renowned conveyor belting company, recognized globally for its innovative solutions in material handling. With a rich history spanning decades, Fenner Dunlop pioneers advancements in conveyor technology, ensuring reliability, efficiency, and safety in industries ranging from mining and agriculture to manufacturing and logistics. Committed to excellence, Fenner Dunlop's high-quality belting products and comprehensive services empower businesses worldwide to optimize their operations and achieve sustainable success in today's dynamic market landscape.",
+    img: "https://static.wixstatic.com/media/2b8fca_5452e0049b7e4d6091f2e71bd46a95e2~mv2.gif",
+    id: 1,
+  },
+  {
+    title: "Flexco",
+    description:
+      "When an unexpected shut down occurs because of a conveyor belt problem, your productivity suffers. Fortunately, Flexco can help you get up and running fast. We offer a wide array of quick and easy belt splicing solutions, including long-lasting mechanical fasteners and reliable endless splicing tools. So whether you’re moving apples or aggregate, there's a Flexco product that helps ensure you'll get maximum uptime from your belt line.",
+    img: "https://static.wixstatic.com/media/2b8fca_781b7167058f449aa37eafd0704ea90d~mv2.gif",
+    id: 2,
+  },
+  {
+    title: "Icl America Limited",
+    description:
+      " ICL AMERICA LIMITED is involved in the sales and marketing of Fabric Rubber Conveyor Belting, servicing bulk material handling industry requirements. All Synthetic constructions provide higher adhesion, excellent resistance to Edge wear, higher impact resistance, Superior tear and longitudinal rip-resistance, excellent rot/mildew resistance, higher breaking strength, deep troughing and improved tracking, controlled elongation.",
+    img: "https://static.wixstatic.com/media/2b8fca_2cee23fe51f242bc993b03b7d5f54c2a~mv2.jpg/v1/fill/w_402,h_101,al_c,lg_1,q_80,enc_auto/2b8fca_2cee23fe51f242bc993b03b7d5f54c2a~mv2.jpg",
+    id: 3,
+  },
+  {
+    title: "Asgco",
+    description:
+      "To Provide the World’s Material Handling Industry with Efficient, Safe and Productive “Complete Conveyor Solutions”. ASGCO® “Complete Conveyor Solutions”, founded in 1971 and headquartered in Allentown, PA is a leading global manufacturer and service provider of proprietary bulk conveyor components and systems. We use proven products, practical engineering and turn-key service solutions to increase conveyor efficiencies and production capabilities while improving safe work environment.",
+    img: "https://static.wixstatic.com/media/2b8fca_a42b7b92c91e40caaa902253a5912844~mv2.jpg/v1/crop/x_0,y_41,w_154,h_62/fill/w_211,h_87,al_c,lg_1,q_80,enc_auto/2b8fca_a42b7b92c91e40caaa902253a5912844~mv2.jpg",
+    id: 4,
+  },
+  {
+    title: "Forbo",
+    description:
+      "The Movement Systems division is a global industry leader in supplying high-quality conveyor belts and processing belts as well as plastic modular belts, power transmission belts, and timing and flat belts made of synthetic materials. They are used in a variety of ways in manufacturing and in the retail and service sectors, for example as conveyor and processing belts in the food industry, as treadmills in fitness studios or as flat belts in letter sorting systems.",
+    img: "https://static.wixstatic.com/media/2b8fca_4254b96b79a6410a9716fdbd195b0fc9~mv2.jpg/v1/crop/x_13,y_43,w_227,h_102/fill/w_309,h_143,al_c,lg_1,q_80,enc_auto/2b8fca_4254b96b79a6410a9716fdbd195b0fc9~mv2.jpg",
+    id: 5,
+  },
+  {
+    title: "Ammeraal Beltech",
+    description:
+      " Ammeraal Beltech offers the largest range of belting products worldwide to over 150 countries. A truly global company, we have production plants in 7 countries, over 80 service centres, more than 2,500 employees and 25,000 customers in 26 different countries. It’s no wonder therefore that we’re well-known and respected in the world of belting. Indeed, we have built up a reputation as specialists for the innovative design and manufacture of a variety of belting systems for very diverse markets. These include food, airports, logistics & mail, tobacco, paper & print, textile, automotive, rubber & tyre and wood.",
+    img: "https://static.wixstatic.com/media/2b8fca_aca333d4e2e7448e8ebf868fcb16f32c~mv2.jpg/v1/fill/w_287,h_101,al_c,lg_1,q_80,enc_auto/2b8fca_aca333d4e2e7448e8ebf868fcb16f32c~mv2.jpg",
+    id: 6,
+  },
+];
+
 function About() {
   return (
-    <section className="about">
-      <SectionHeader title="about us" />
-      <p className="about__description">
-        Western Belting Ltd was established in 1959 with one mission: to bring a
-        real high quality of support, products and service to the industry. Our
-        passion for excellence is what inspired us in the beginning and it
-        continues to drive us today. We pride ourselves in the superior
-        experience in Conveyor, Material Handling and in the long-term
-        relationships we’ve built with our customers. ​ People come back to
-        Western Belting Ltd because they know they’ll find what they’re looking
-        for, if they don’t, we’ll help them find it!
+    <section className="suppliers">
+      <h2 className="section__title">Suppliers</h2>
+      <p className="section__description">
+        Discover our trusted suppliers section, where we partner with
+        industry-leading providers to ensure premium-quality materials and
+        services. Our rigorous selection process guarantees reliability,
+        efficiency, and unmatched performance, empowering your projects with the
+        best-in-class solutions.
       </p>
+      <div className="suppliers__container">
+        {suppliersData.map((supplier) => (
+          <article className="suppliers__card" key={supplier.id}>
+            <span className="suppliers__number">{supplier.id}/6</span>
+            <div className="suppliers__img-container">
+              <img src={supplier.img} alt="" className="suppliers__img" />
+            </div>
+            <div className="suppliers__data">
+              <h3 className="suppliers__title">{supplier.title}</h3>
+              <p className="suppliers__description">{supplier.description}</p>
+            </div>
+          </article>
+        ))}
+      </div>
     </section>
   );
 }
