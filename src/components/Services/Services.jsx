9588fb3620e464f7f3e8b7d25fbe4894437e2a9d
thisclinton <thisclinton@gmail.com>
2024-01-30 18:32:14 +0100

    refactored code into an array of objects for a more cleaner code block

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -2,6 +2,57 @@ import { motion } from "framer-motion";
 import "./services.css";
 import Transition from "../../transition";
 
+const services = [
+  {
+    title: "Vulcanizing",
+    description:
+      "The preferred method of joining two ends of a belt together. With vulcanizing a belt, you attain the longest life possible from that splice.",
+    id: 1,
+  },
+  {
+    title: "Pulley Lagging",
+    description:
+      "Pulley lagging is a protective layer applied to conveyor pulleys, enhancing traction between the belt and pulley surface, reducing slippage, wear, and increasing efficiency.",
+    id: 2,
+  },
+  {
+    title: "Lacing",
+    description:
+      "This is a method of joining conveyor belts using metal or plastic fasteners, ensuring secure and flexible connections, facilitating belt repairs and maintenance in industrial applications.",
+    id: 3,
+  },
+  {
+    title: "Tracking",
+    description:
+      "Tracking in conveyor systems refers to the alignment and adjustment of belts to ensure they run smoothly, preventing misalignment, reducing wear, and optimizing performance in material handling operations.",
+    id: 4,
+  },
+  {
+    title: "Stringing",
+    description:
+      "Stringing in conveyor systems is the process of threading or installing a conveyor belt onto its pulleys, rollers, and idlers, ensuring proper alignment and tension for efficient operation.",
+    id: 5,
+  },
+  {
+    title: "Patching",
+    description:
+      "Patching involves repairing damaged areas on conveyor belts using specialized materials or techniques, extending their lifespan, maintaining operational integrity, and minimizing downtime in industrial settings.",
+    id: 6,
+  },
+  {
+    title: "Steel Cable Splicing",
+    description:
+      "Steel cable splicing is the process of joining individual strands of steel cables to form a continuous loop, ensuring strength and reliability in applications like lifting, rigging, and suspension systems.",
+    id: 7,
+  },
+  {
+    title: "Lightweight / Specialty",
+    description:
+      "Lightweight or specialty conveyor belting refers to belts designed for specific applications such as food processing, packaging, or high-temperature environments, offering flexibility, durability, and customization for specialized industrial needs.",
+    id: 8,
+  },
+];
+
 function Services() {
   return (
     <>
@@ -24,85 +75,13 @@ function Services() {
             </p>
 
             <div className="services__data">
-              <article class="services__card">
-                <h3 class="services__title">Vulcanizing</h3>
-                <span class="services__info">
-                  The preferred method of joining two ends of a belt together.
-                  With vulcanizing a belt, you attain the longest life possible
-                  from that splice.
-                </span>
-                <span className="services__btn">Learn More</span>
-              </article>
-
-              <article class="services__card">
-                <h3 class="services__title">Pulley Lagging</h3>
-                <span class="services__info">
-                  The preferred method of joining two ends of a belt together.
-                  With vulcanizing a belt, you attain the longest life possible
-                  from that splice.
-                </span>
-                <span className="services__btn">Learn More</span>
-              </article>
-
-              <article class="services__card">
-                <h3 class="services__title">Lacing</h3>
-                <span class="services__info">
-                  The preferred method of joining two ends of a belt together.
-                  With vulcanizing a belt, you attain the longest life possible
-                  from that splice.
-                </span>
-                <span className="services__btn">Learn More</span>
-              </article>
-
-              <article class="services__card">
-                <h3 class="services__title">Tracking</h3>
-                <span class="services__info">
-                  The preferred method of joining two ends of a belt together.
-                  With vulcanizing a belt, you attain the longest life possible
-                  from that splice.
-                </span>
-                <span className="services__btn">Learn More</span>
-              </article>
-
-              <article class="services__card">
-                <h3 class="services__title">Stringing</h3>
-                <span class="services__info">
-                  The preferred method of joining two ends of a belt together.
-                  With vulcanizing a belt, you attain the longest life possible
-                  from that splice.
-                </span>
-                <span className="services__btn">Learn More</span>
-              </article>
-
-              <article class="services__card">
-                <h3 class="services__title">Patching</h3>
-                <span class="services__info">
-                  The preferred method of joining two ends of a belt together.
-                  With vulcanizing a belt, you attain the longest life possible
-                  from that splice.
-                </span>
-                <span className="services__btn">Learn More</span>
-              </article>
-
-              <article class="services__card">
-                <h3 class="services__title">Steel Cable Splicing</h3>
-                <span class="services__info">
-                  The preferred method of joining two ends of a belt together.
-                  With vulcanizing a belt, you attain the longest life possible
-                  from that splice.
-                </span>
-                <span className="services__btn">Learn More</span>
-              </article>
-
-              <article class="services__card">
-                <h3 class="services__title">Lightweight/Specialty</h3>
-                <span class="services__info">
-                  The preferred method of joining two ends of a belt together.
-                  With vulcanizing a belt, you attain the longest life possible
-                  from that splice.
-                </span>
-                <span className="services__btn">Learn More</span>
-              </article>
+              {services.map((service) => (
+                <article class="services__card" key={service.id}>
+                  <h3 class="services__title">{service.title}</h3>
+                  <span class="services__info">{service.description}</span>
+                  <span className="services__btn">Learn More</span>
+                </article>
+              ))}
             </div>
           </div>
         </div>
