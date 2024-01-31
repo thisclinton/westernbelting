dac553b3665e104a2c9799c66881eec88149cca4
thisclinton <thisclinton@gmail.com>
2024-01-31 13:55:39 +0100

    worked on animating the nav links

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,107 +1,72 @@
 import { useState } from "react";
 import "./Nav.css";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+
+const navVariants = {
+  hidden: { opacity: 0, y: 50 },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      staggerChildren: 0.25,
+      duration: 1.3,
+      delay: 4,
+      ease: "easeOut",
+    },
+  },
+};
+
+const navLinksVariants = {
+  hidden: { opacity: 0 },
+  show: { opacity: 1 },
+};
+
+const navs = [
+  { title: "products", link: "/products", id: 1 },
+  { title: "services", link: "/services", id: 2 },
+  { title: "our workshops", link: "/workshop", id: 3 },
+  { title: "safety", link: "/safety", id: 4 },
+  { title: "shop", link: "/shop", id: 5 },
+  { title: "about us", link: "/about", id: 6 },
+];
 
 function Nav() {
   const [nav, setNav] = useState(false);
-
-  function changeBackground() {
-    if (window.scrollY >= 80) {
-      setNav(true);
-    } else {
-      setNav(false);
-    }
-  }
-
-  window.addEventListener("scroll", changeBackground);
 
   return (
     <>
       <header className={nav ? "header active" : "header"}>
         <nav className="nav">
-          <Link to="/" className="nav__logo">
-            Western <span>BELTING</span>
-          </Link>
+          <motion.div
+            className="nav__logo-container"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1, transition: { duration: 1.7, delay: 4 } }}
+          >
+            <Link to="/" className="nav__logo">
+              Western <span>BELTING</span>
+            </Link>
+          </motion.div>
 
           <div className="nav__menu">
-            <ul className="nav__list">
-              <li className="nav__item">
-                <Link to="/products" className="nav__link">
-                  Products
-                </Link>
-                {/* <ul className="nav__dropmenu">
-                  <li>
-                    <a href="#">Heavy Duty Belting</a>
-                  </li>
-                  <li>
-                    <a href="#">Hercules Belt Winder</a>
-                  </li>
-                  <li>
-                    <a href="#">Just-In Conveyor Belt Scraper</a>
-                  </li>
-                  <li>
-                    <a href="#">Piranha Plate Protection Products</a>
-                  </li>
-                </ul> */}
-              </li>
-
-              <li className="nav__item">
-                <Link to="/services" className="nav__link">
-                  Services
-                </Link>
-
-                {/*  <ul className="nav__dropmenu">
-                  <li>
-                    <a href="#">Vulcanizing</a>
-                  </li>
-                  <li>
-                    <a href="#">Pulley Lagging</a>
-                  </li>
-                  <li>
-                    <a href="#">Lacing</a>
-                  </li>
-                  <li>
-                    <a href="#">Tracking</a>
-                  </li>
-                  <li>
-                    <a href="#">Stringing</a>
-                  </li>
-                  <li>
-                    <a href="#">Patching</a>
-                  </li>
-                  <li>
-                    <a href="#">Steel Cable Splicing</a>
-                  </li>
-                  <li>
-                    <a href="#">Lightweight Speciality</a>
-                  </li>
-                </ul> */}
-              </li>
-
-              <li className="nav__item">
-                <Link to="/workshop" className="nav__link">
-                  Our Workshops
-                </Link>
-              </li>
-
-              <li className="nav__item">
-                <Link to="/safety" className="nav__link">
-                  Safety
-                </Link>
-              </li>
-
-              <li className="nav__item">
-                <Link to="/shop" className="nav__link">
-                  Shop
-                </Link>
-              </li>
-
-              <li className="nav__item">
-                <Link to="/about" className="nav__link">
-                  About Us
-                </Link>
-              </li>
-            </ul>
+            <motion.ul
+              className="nav__list"
+              variants={navVariants}
+              initial="hidden"
+              animate="show"
+            >
+              {navs.map((nav) => (
+                <motion.li
+                  className="nav__item"
+                  key={nav.id}
+                  variants={navLinksVariants}
+                >
+                  <Link to={nav.link} className="nav__link">
+                    {nav.title}
+                  </Link>
+                </motion.li>
+              ))}
+            </motion.ul>
           </div>
 
           <div className="nav__contact">
