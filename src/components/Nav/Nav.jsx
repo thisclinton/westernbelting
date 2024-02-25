f7479ef0bfe5c5572d24b5ecf64ff710bbb9b9a0
thisclinton <thisclinton@gmail.com>
2024-02-25 02:32:20 +0100

    update

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -31,7 +31,7 @@ const navs = [
   { title: "health & safety", link: "/safety", id: 4 },
   { title: "industries", link: "/industries", id: 5 },
   { title: "news", link: "/blog", id: 6 },
-  { title: "career", link: "/career", id: 7 },
+  { title: "careers", link: "/career", id: 7 },
 ];
 
 function Nav() {
